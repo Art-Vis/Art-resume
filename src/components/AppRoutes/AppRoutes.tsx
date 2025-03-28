fa4c7ae2abf71d0cf87809a09bcc683fca7a83e3
Art Vis <artyom0698@bk.ru>
2025-03-28 15:15:57 +0300

    repair v2

--- src/components/AppRoutes/AppRoutes.tsx
+++ src/components/AppRoutes/AppRoutes.tsx
@@ -1,7 +1,5 @@
 import { FC, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Login from '../Pages/Login/Login';
-import AdminPanel from '../Pages/AdminPanel/AdminPanel';
 const LazyProfilePage = lazy(() => import('@pages/Profile/Profile'));
 const LazyTests = lazy(() => import('@pages/MyTests/MyTest'));
 const LazyFrontendTest = lazy(
@@ -12,8 +10,6 @@ const AppRoutes: FC = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<LazyProfilePage />} />
-			<Route path='/login' element={<Login />} />
-			<Route path='/admin' element={<AdminPanel />} />
 			<Route path='/my-tests' element={<LazyTests />} />
 			<Route path='/frontend-test' element={<LazyFrontendTest />} />
 		</Routes>
